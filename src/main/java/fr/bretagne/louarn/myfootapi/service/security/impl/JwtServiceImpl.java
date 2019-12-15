471152555624c6143f77fcf99b30aa6aa18c9ad4
Robin LOUARN <robin.louarn@icloud.com>
2019-12-15 01:19:30 +0100

    Correction auth

--- src/main/java/fr/bretagne/louarn/myfootapi/service/security/impl/JwtServiceImpl.java
+++ src/main/java/fr/bretagne/louarn/myfootapi/service/security/impl/JwtServiceImpl.java
@@ -54,9 +54,13 @@ public class JwtServiceImpl implements IJwtservice {
     }
 
     private String createToken(Map<String, Object> claims, String subject) {
-        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
-                .signWith(SignatureAlgorithm.HS256, appProperties.getSecret()).compact();
+                .signWith(SignatureAlgorithm.HS256, appProperties.getSecret().getBytes())
+                .compact();
     }
 
     @Override
